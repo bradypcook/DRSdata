143cfff2beaa4fce685bde5a7a2eea83aa74b80e
LilCook <bcook468@gmail.com>
2025-06-21 11:45:30 -0400

    Files

--- drsdata/next.config.ts
+++ drsdata/next.config.ts
@@ -1,7 +1,11 @@
  // import type { NextConfig } from "next"; - ARCHIVED
 
+const repo = "DRSdata";
+
 const nextConfig = {
   output: 'export',
+  basePath: `/${repo}`,
+  assetPrefix: `/${repo}/`,
 };
 
 export default nextConfig;
